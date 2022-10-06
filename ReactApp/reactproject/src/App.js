98defa7f6345a2b3784b73bab1a0fe2f05d0b4aa
aruna-jothi <jothimarch7@gmail.com>
2022-10-06 21:29:21 +0530

    final code

--- ReactApp/reactproject/src/App.js
+++ ReactApp/reactproject/src/App.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
-
 function App() {
     const [counter, setcounter] = useState(0);
     const increase = () => {
-        setcounter(count => count + 1)
+        setcounter(counter + 1)
     }
     const decrease = () => {
         if (counter > 0) {
-            setcounter(count => count - 1)
+            setcounter(counter- 1)
         }
     }
 
-    return ( 
-        <div className = "App" >
-        <h1> React Counter App </h1> 
-        <div> { counter } </div> 
-        <button onClick = { increase } > Increment </button> 
-        <button onClick = { decrease } > Decrement </button> 
+    return (
+        <div className="App" >
+            <h1> React Counter App </h1>
+            <div> {counter} </div>
+            <button onClick={increase} > Increment </button>
+            <button onClick={decrease} > Decrement </button>
         </div>
     );
 }
